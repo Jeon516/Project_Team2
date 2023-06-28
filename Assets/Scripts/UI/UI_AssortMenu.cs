663ede85a94fae65cd9a3789c7b3b27eb1f3295e
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-06-28 17:22:40 +0900

    Revert "육성 씬 UI 작성"
    This reverts commit 134ff84c27db2e271fad014dbf6572c28dfd7bbf.

--- Assets/Scripts/UI/UI_AssortMenu.cs
+++ Assets/Scripts/UI/UI_AssortMenu.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class UI_AssortMenu : MonoBehaviour
 {
     public GameObject Menu;
+    public GameObject Setting;
 
+    public void OnClick_SettingOpen()
+    {
+        Setting.SetActive(true);
+    }
     public void OnClick_MeunOpen()
     {
         Menu.SetActive(true);
     }
-    public void OnClick_MeunClose()
+    public void OnClick_MenuClose()
     {
         Menu.SetActive(false);
     }

887865bdaeaf4f57b7d1e6c41d9b6c9148b6eae0
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-06-29 08:37:23 +0900

    데이터 역직렬화 작업

--- Assets/DataFile/JSON_Manager.cs
+++ Assets/DataFile/JSON_Manager.cs
@@ -1,79 +1,75 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
 public class JSON_Manager : MonoBehaviour
 {
-    public string jsonFilePath = "Assets/DataFile/foodinfo.json"; // JSON Data Path
+    public string jsonFilePath = "DataFile/foodinfo.json"; // JSON Data Path
 
     void Start()
     {
         // JSON Load
-        string jsonText = LoadJSONFromFile(jsonFilePath);
-
-        if (string.IsNullOrEmpty(jsonText))
-        {
-            Debug.Log("Error loading JSON file.");
-            return;
-        }
-
-        // JSON to Object
-        MyDataObject dataObject = JsonUtility.FromJson<MyDataObject>(jsonText);
-
-        // Data loging
-        Debug.Log("Class: " + dataObject.myClass);
-        Debug.Log("Sort: " + dataObject.sort);
-        Debug.Log("Name: " + dataObject.name);
-        Debug.Log("Item Text: " + dataObject.itemText);
-        Debug.Log("Conv: " + dataObject.conv);
-        Debug.Log("1st Stat Type: " + dataObject.firstStatType);
-        Debug.Log("1st Stat Value: " + dataObject.firstStatValue);
-        Debug.Log("2nd Stat Type: " + dataObject.secondStatType);
-        Debug.Log("2nd Stat Value: " + dataObject.secondStatValue);
-        Debug.Log("Probability: " + dataObject.probability);
-
-        // Additional Logic Adjustment
+        LoadJSONFromFile(jsonFilePath);
     }
 
     // Load JSON From File
-    string LoadJSONFromFile(string filePath)
+    void LoadJSONFromFile(string filePath)
     {
         string jsonText = null;
 
-        try
-        {
-            string fullPath = Path.Combine(Application.streamingAssetsPath, filePath);
+        string fullPath = Path.Combine(Application.dataPath, filePath);
 
-            if (File.Exists(fullPath))
-            {
-                jsonText = File.ReadAllText(fullPath);
-            }
-            else
-            {
-                Debug.Log("File does not exist: " + filePath);
-            }
-        }
-        catch (System.Exception e)
+        if (File.Exists(fullPath))
         {
-            Debug.Log("Error loading JSON file: " + e.Message);
-        }
+            jsonText = File.ReadAllText(fullPath);
 
-        return jsonText;
+            // JSON to Object
+            MyDataWrapper dataWrapper = JsonUtility.FromJson<MyDataWrapper>(jsonText);
+            List<MyDataObject> dataObjects = dataWrapper.data;
+
+            // Data logging
+            foreach (MyDataObject dataObject in dataObjects)
+            {
+                Debug.Log("Num: " + dataObject.num);
+                Debug.Log("Class: " + dataObject.myClass);
+                Debug.Log("Sort: " + dataObject.sort);
+                Debug.Log("Name: " + dataObject.name);
+                Debug.Log("Item Text: " + dataObject.itemText);
+                Debug.Log("Conv: " + dataObject.conv);
+                Debug.Log("1st Stat Type: " + dataObject.firstStatType);
+                Debug.Log("1st Stat Value: " + dataObject.firstStatValue);
+                Debug.Log("2nd Stat Type: " + dataObject.secondStatType);
+                Debug.Log("2nd Stat Value: " + dataObject.secondStatValue);
+                Debug.Log("Probability: " + dataObject.probability);
+            }
+
+            // Additional Logic Adjustment
+        }
+        else
+        {
+            Debug.Log("Error loading JSON file: File not found");
+        }
     }
+}
+
+[System.Serializable]
+public class MyDataWrapper
+{
+    public List<MyDataObject> data;
 }
 
 [System.Serializable]
 public class MyDataObject
 {
+    public int num;
     public string myClass;
     public string sort;
     public string name;
     public string itemText;
     public string conv;
-    public string firstStatType;
+    public int firstStatType;
     public int firstStatValue;
-    public string secondStatType;
+    public int secondStatType;
     public int secondStatValue;
     public float probability;
 }

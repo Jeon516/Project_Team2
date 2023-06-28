e8d69df1c16f0a25e79b4405a6d8b594698491f7
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-06-28 17:23:57 +0900

    Revert "Revert "육성 씬 UI 작성""
    This reverts commit 663ede85a94fae65cd9a3789c7b3b27eb1f3295e.

---
+++ Assets/Scripts/UI/UI_MenuSetting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MenuSetting : MonoBehaviour
+{
+    public GameObject Setting;
+
+    public void OnClick_Setting()
+    {
+        Setting.SetActive(true);
+    }
+}

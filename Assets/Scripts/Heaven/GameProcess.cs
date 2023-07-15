a70031d21aef2b14e1a9af87100248bdd97c430d
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-07-16 02:50:21 +0900

    최종 확률 검토 전

--- Assets/Scripts/Heaven/GameProcess.cs
+++ Assets/Scripts/Heaven/GameProcess.cs
@@ -20,6 +20,8 @@ public class GameProcess : MonoBehaviour
     private List<Sprite> trainRSprites = new List<Sprite>();
     private List<Sprite> ticketSprites = new List<Sprite>();
 
+    private List<string> availableColors = new List<string>();
+
     private void Start()
     {
         dayValue = HeavenGameManager.Instance.Day;
@@ -36,63 +38,51 @@ public class GameProcess : MonoBehaviour
         if (File.Exists(JSONFullPath))
         {
             string json = File.ReadAllText(JSONFullPath);
-
             SetDataList setDataList = JsonUtility.FromJson<SetDataList>(json);
-
-            if (dayValue >= 1 && dayValue <= 5)
-            {
-                TL = 40;
-                TR = 40;
-                R = 20;
-            }
-            else if (dayValue >= 6 && dayValue <= 15)
-            {
-                TL--;
-                TR--;
-                R += 2;
-            }
-            else if (dayValue >= 16 && dayValue <= 20)
-            {
-                TL = 30;
-                TR = 30;
-                R = 40;
-            }
 
             if (setDataList != null && setDataList.Sets.Count > 0)
             {
-                // Color 필드 랜덤 선택
-                List<string> availableColors = new List<string>();
                 foreach (SetData setData in setDataList.Sets)
                 {
                     availableColors.Add(setData.Color);
                 }
 
+                if (dayValue >= 1 && dayValue <= 5)
+                {
+                    TL = 40;
+                    TR = 40;
+                    R = 20;
+                }
+                else if (dayValue >= 6 && dayValue <= 15)
+                {
+                    TL--;
+                    TR--;
+                    R += 2;
+                }
+                else if (dayValue >= 16 && dayValue <= 20)
+                {
+                    TL = 30;
+                    TR = 30;
+                    R = 40;
+                }
+
                 if (availableColors.Count >= 3)
                 {
-                    int randomIndex1 = Random.Range(0, availableColors.Count);
-                    string randomColor1 = availableColors[randomIndex1];
-                    availableColors.RemoveAt(randomIndex1);
+                    // Fisher-Yates 알고리즘을 사용하여 availableColors 리스트를 섞습니다.
+                    int n = availableColors.Count;
+                    while (n > 1)
+                    {
+                        n--;
+                        int k = Random.Range(0, n + 1);
+                        string value = availableColors[k];
+                        availableColors[k] = availableColors[n];
+                        availableColors[n] = value;
+                    }
 
-                    int randomIndex2 = Random.Range(0, availableColors.Count);
-                    string randomColor2 = availableColors[randomIndex2];
-                    availableColors.RemoveAt(randomIndex2);
-
-                    string randomColor3;
-                    int randomValue = Random.Range(1, 101);
-                    if (randomValue <= TL)
-                    {
-                        randomColor3 = randomColor1;
-                    }
-                    else if (randomValue <= TL + TR)
-                    {
-                        randomColor3 = randomColor2;
-                    }
-                    else
-                    {
-                        List<string> remainingColors = availableColors.FindAll(color => color != randomColor1 && color != randomColor2);
-                        int remainingIndex = Random.Range(0, remainingColors.Count);
-                        randomColor3 = remainingColors[remainingIndex];
-                    }
+                    // 첫 3개의 색상을 가져옵니다.
+                    string randomColor1 = availableColors[0];
+                    string randomColor2 = availableColors[1];
+                    string randomColor3 = availableColors[2];
 
                     // 랜덤 컬러를 파일 경로에 할당
                     string TrainLPath = "Image/Heaven/Train(Size)/LeftSide/" + randomColor1;
@@ -108,6 +98,11 @@ public class GameProcess : MonoBehaviour
                     SetRandomImage(TrainLeft, trainLSprites);
                     SetRandomImage(TrainRight, trainRSprites);
                     SetRandomImage(Ticket, ticketSprites);
+
+                    // 로그 출력
+                    Debug.Log("Random Color 1: " + randomColor1);
+                    Debug.Log("Random Color 2: " + randomColor2);
+                    Debug.Log("Random Color 3: " + randomColor3);
                 }
                 else
                 {
@@ -158,10 +153,50 @@ public class GameProcess : MonoBehaviour
         int randomIndex1 = Random.Range(0, trainLSprites.Count);
         int randomIndex2 = Random.Range(0, trainRSprites.Count);
 
+        // randomIndex1과 randomIndex2가 서로 다른 값이 될 때까지 반복합니다.
+        while (randomIndex1 == randomIndex2)
+        {
+            randomIndex2 = Random.Range(0, trainRSprites.Count);
+        }
+
         // 이미지 할당
         TrainLeft.sprite = trainLSprites[randomIndex1];
         TrainRight.sprite = trainRSprites[randomIndex2];
-        SetRandomImage(Ticket, ticketSprites);
+
+        // 로그 출력
+        string randomColor1 = availableColors[randomIndex1];
+        string randomColor2 = availableColors[randomIndex2];
+        Debug.Log("Random Color 1: " + randomColor1);
+        Debug.Log("Random Color 2: " + randomColor2);
+
+        // randomColor3 선택
+        string randomColor3;
+        int randomValue = Random.Range(1, 101);
+        if (randomValue <= TL)
+        {
+            randomColor3 = randomColor1;
+        }
+        else if (randomValue <= TL + TR)
+        {
+            randomColor3 = randomColor2;
+        }
+        else
+        {
+            List<string> remainingColors = availableColors.FindAll(color => color != randomColor1 && color != randomColor2);
+            int remainingIndex = Random.Range(0, remainingColors.Count);
+            randomColor3 = remainingColors[remainingIndex];
+        }
+        Debug.Log("Random Color 3: " + randomColor3);
+
+    Ticket.sprite = GetTicketSprite(randomColor3);
+    }
+
+    private Sprite GetTicketSprite(string color)
+    {
+        // color에 해당하는 Ticket 이미지를 리소스에서 찾아 반환합니다.
+        string path = "Image/Heaven/tickets/" + color;
+        Sprite ticketSprite = Resources.Load<Sprite>(path);
+        return ticketSprite;
     }
 }
 

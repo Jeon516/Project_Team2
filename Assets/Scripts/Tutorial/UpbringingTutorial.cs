32626f313a276a40c28ee4813727c871337616b1
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-07-23 00:01:58 +0900

    수정

---
+++ Assets/Scripts/Tutorial/UpbringingTutorial.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpbringingTutorial : MonoBehaviour
+{
+    private string[] FirstChat = {  "유령을 돌보는 기간은 20일을 생각하고 있습니다.","이 유령들은 20일 정도의 기간동안 돌보다 보면 기억과 함께 모습을 되찾더군요.",
+    "특히, 어떤 성향을 가지냐에 따라 모습이 달라지는 것 같았습니다.", "만약 이 가설이 사실이라면, 상점에서 판매하는 특별한 음식으로 유령의 기억을 더 정확하게 찾게 해줄 수 있을 겁니다.",
+        "말이 나온 김에 바로 상점을 이용해봅시다.","각각의 음식 주문은 하루에 한 번씩만 가능하니 유의하시기 바랍니다.", "딱 이번만 비용을 대신 내드릴 테니 한 번 주문해보세요."};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

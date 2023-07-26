839fdc32d3486d930e90b0b31182af09af5e6a08
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-07-26 14:57:18 +0900

    인트로씬 마무리

--- Assets/Resources/Image/IntroScene/FirstScene.cs
+++ Assets/Resources/Image/IntroScene/FirstScene.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 [System.Serializable]
 public class ImageData
@@ -16,20 +16,24 @@ public class FirstScene : MonoBehaviour
     public Button nextButton;
     public Image blurTargetImage;
     public Material blurMaterial;
-    public float blurStep = 1.0f;
-    public float transitionTimeIndex1 = 5.0f;
+    public float transitionTimeIndex1 = 2.0f;
+    public float maxBlurRadius = 255.0f;
+    public float minBlurRadius = 0.0f;
+    public float pauseTimeAfterIndex1 = 3.0f;
+    public float transitionTimeIndex3 = 5.0f;
 
     private int currentImageIndex = 0;
     private bool isTransitioning = false;
-    private float currentBlurValue = 255.0f;
-
+    private bool isSceneTransitionAllowed = true;
     private Coroutine transitionCoroutine;
+    private Coroutine colorTransitionCoroutine;
 
     void Start()
     {
         ShowImage(currentImageIndex);
 
-        blurMaterial = blurTargetImage.material;
+        blurMaterial = new Material(blurMaterial);
+        blurTargetImage.material = blurMaterial;
 
         if (nextButton != null)
         {
@@ -38,46 +42,115 @@ public class FirstScene : MonoBehaviour
         }
     }
 
-    private IEnumerator TransitionBlurEffect(float targetBlurValue, float transitionTime)
+    private IEnumerator TransitionBlurEffect(float startBlurValue, float targetBlurValue, float transitionTime)
     {
         isTransitioning = true;
         float elapsedTime = 0.0f;
-        float startBlurValue = currentBlurValue;
 
         while (elapsedTime < transitionTime)
         {
-            currentBlurValue = Mathf.Lerp(startBlurValue, targetBlurValue, elapsedTime / transitionTime);
+            float currentBlurValue = Mathf.Lerp(startBlurValue, targetBlurValue, elapsedTime / transitionTime);
             blurMaterial.SetFloat("_Radius", currentBlurValue);
+
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        currentBlurValue = targetBlurValue;
-        blurMaterial.SetFloat("_Radius", currentBlurValue);
+        blurMaterial.SetFloat("_Radius", targetBlurValue);
         isTransitioning = false;
+    }
+
+    private IEnumerator PauseAfterIndex1(float pauseTime)
+    {
+        isSceneTransitionAllowed = false; // Disable scene transition during the pause
+        nextButton.interactable = false; // Disable the nextButton during the pause
+
+        yield return new WaitForSeconds(pauseTime);
+
+        isSceneTransitionAllowed = true; // Re-enable scene transition after the pause
+        nextButton.interactable = true; // Re-enable the nextButton after the pause
+
+        StartTransitionToMaxBlur();
     }
 
     private void ShowImage(int index)
     {
         for (int i = 0; i < imagesToDisplay.Length; i++)
         {
-            imagesToDisplay[i].image.gameObject.SetActive(i == index);
+            bool isActive = i == index;
+            imagesToDisplay[i].image.gameObject.SetActive(isActive);
+
+            if (isActive)
+            {
+                if (index == 1)
+                {
+                    if (transitionCoroutine != null)
+                    {
+                        StopCoroutine(transitionCoroutine);
+                    }
+
+                    transitionCoroutine = StartCoroutine(TransitionBlurEffect(maxBlurRadius, minBlurRadius, transitionTimeIndex1));
+                    StartCoroutine(PauseAfterIndex1(transitionTimeIndex1 + pauseTimeAfterIndex1));
+                }
+                else if (index == 3)
+                {
+                    if (transitionCoroutine != null)
+                    {
+                        StopCoroutine(transitionCoroutine);
+                    }
+
+                    if (colorTransitionCoroutine != null)
+                    {
+                        StopCoroutine(colorTransitionCoroutine);
+                    }
+
+                    transitionCoroutine = StartCoroutine(TransitionBlurEffect(maxBlurRadius, minBlurRadius, transitionTimeIndex3));
+                    colorTransitionCoroutine = StartCoroutine(TransitionColorEffect(Color.black, Color.white, transitionTimeIndex3));
+                }
+                else
+                {
+                    if (transitionCoroutine != null)
+                    {
+                        StopCoroutine(transitionCoroutine);
+                    }
+
+                    blurMaterial.SetFloat("_Radius", minBlurRadius);
+                }
+            }
         }
+    }
+
+    private void StartTransitionToMaxBlur()
+    {
+        if (transitionCoroutine != null)
+        {
+            StopCoroutine(transitionCoroutine);
+        }
+
+        transitionCoroutine = StartCoroutine(TransitionBlurEffect(minBlurRadius, maxBlurRadius, transitionTimeIndex1));
+    }
+
+    private IEnumerator TransitionColorEffect(Color startColor, Color targetColor, float transitionTime)
+    {
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < transitionTime)
+        {
+            Color currentColor = Color.Lerp(startColor, targetColor, elapsedTime / transitionTime);
+            imagesToDisplay[3].image.color = currentColor;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        imagesToDisplay[3].image.color = targetColor;
     }
 
     public void ShowNextImage()
     {
-        if (isTransitioning || !imagesToDisplay[currentImageIndex].useInTransition)
+        if (!isSceneTransitionAllowed || isTransitioning || !imagesToDisplay[currentImageIndex].useInTransition)
         {
-            return; // 이미지 전환 중이거나 전환에 사용되지 않는 이미지라면 무시
-        }
-
-        float targetBlurValue = 1.0f;
-        float transitionTime = transitionTimeIndex1;
-
-        if (currentImageIndex == 1 || currentImageIndex == 2)
-        {
-            targetBlurValue = 1.0f;
+            return;
         }
 
         if (transitionCoroutine != null)
@@ -85,13 +158,12 @@ public class FirstScene : MonoBehaviour
             StopCoroutine(transitionCoroutine);
         }
 
-        transitionCoroutine = StartCoroutine(TransitionBlurEffect(targetBlurValue, transitionTime));
-
-        currentImageIndex = (currentImageIndex + 1);
-        if (currentImageIndex == imagesToDisplay.Length)
+        if (colorTransitionCoroutine != null)
         {
-            SceneManager.LoadScene("Tutorial");
-        } // next Scene
+            StopCoroutine(colorTransitionCoroutine);
+        }
+
+        currentImageIndex = (currentImageIndex + 1) % imagesToDisplay.Length;
         ShowImage(currentImageIndex);
     }
 }
--- Assets/Resources/Image/IntroScene/blur.shader
+++ Assets/Resources/Image/IntroScene/blur.shader
@@ -7,18 +7,18 @@ Shader "blur"
 
     Category
     {
-        Tags { "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Opaque" }
+        Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Opaque" }
 
         SubShader
         {
             GrabPass
             {
-                Tags { "LightMode" = "Always" }
+                Tags{ "LightMode" = "Always" }
             }
 
             Pass
             {
-                Tags { "LightMode" = "Always" }
+                Tags{ "LightMode" = "Always" }
 
                 CGPROGRAM
                 #pragma vertex vert
@@ -29,20 +29,26 @@ Shader "blur"
                 struct appdata_t
                 {
                     float4 vertex : POSITION;
-                    float2 texcoord : TEXCOORD0;
+                    float2 texcoord: TEXCOORD0;
                 };
 
                 struct v2f
                 {
-                    float4 vertex : SV_POSITION;
-                    float2 uvgrab : TEXCOORD0;
+                    float4 vertex : POSITION;
+                    float4 uvgrab : TEXCOORD0;
                 };
 
                 v2f vert(appdata_t v)
                 {
                     v2f o;
                     o.vertex = UnityObjectToClipPos(v.vertex);
-                    o.uvgrab = v.texcoord; // Use 2D texture coordinates directly
+                    #if UNITY_UV_STARTS_AT_TOP
+                    float scale = -1.0;
+                    #else
+                    float scale = 1.0;
+                    #endif
+                    o.uvgrab.xy = (float2(o.vertex.x, o.vertex.y*scale) + o.vertex.w) * 0.5;
+                    o.uvgrab.zw = o.vertex.zw;
                     return o;
                 }
 
@@ -50,25 +56,94 @@ Shader "blur"
                 float4 _GrabTexture_TexelSize;
                 float _Radius;
 
-                fixed4 frag(v2f i) : SV_Target
+                half4 frag(v2f i) : COLOR
                 {
-                    fixed4 sum = fixed4(0, 0, 0, 0);
+                    half4 sum = half4(0,0,0,0);
 
-                    #define GRABXYPIXEL(kernelx, kernely) tex2D(_GrabTexture, float2(i.uvgrab.x + _GrabTexture_TexelSize.x * kernelx, i.uvgrab.y + _GrabTexture_TexelSize.y * kernely))
+                    #define GRABXYPIXEL(kernelx, kernely) tex2Dproj( _GrabTexture, UNITY_PROJ_COORD(float4(i.uvgrab.x + _GrabTexture_TexelSize.x * kernelx, i.uvgrab.y + _GrabTexture_TexelSize.y * kernely, i.uvgrab.z, i.uvgrab.w)))
 
                     sum += GRABXYPIXEL(0.0, 0.0);
-                    int measurements = 1;
+                    int measurments = 1;
 
-                    for (float range = 1.0; range <= _Radius; range += 1.0)
+                    for (float range = 0.1f; range <= _Radius; range += 0.1f)
+                    {
+                        sum += GRABXYPIXEL(range, range);
+                        sum += GRABXYPIXEL(range, -range);
+                        sum += GRABXYPIXEL(-range, range);
+                        sum += GRABXYPIXEL(-range, -range);
+                        measurments += 4;
+                    }
+
+                    return sum / measurments;
+                }
+                ENDCG
+            }
+            GrabPass
+            {
+                Tags{ "LightMode" = "Always" }
+            }
+
+            Pass
+            {
+                Tags{ "LightMode" = "Always" }
+
+                CGPROGRAM
+                #pragma vertex vert
+                #pragma fragment frag
+                #pragma fragmentoption ARB_precision_hint_fastest
+                #include "UnityCG.cginc"
+
+                struct appdata_t
+                {
+                    float4 vertex : POSITION;
+                    float2 texcoord: TEXCOORD0;
+                };
+
+                struct v2f
+                {
+                    float4 vertex : POSITION;
+                    float4 uvgrab : TEXCOORD0;
+                };
+
+                v2f vert(appdata_t v)
+                {
+                    v2f o;
+                    o.vertex = UnityObjectToClipPos(v.vertex);
+                    #if UNITY_UV_STARTS_AT_TOP
+                    float scale = -1.0;
+                    #else
+                    float scale = 1.0;
+                    #endif
+                    o.uvgrab.xy = (float2(o.vertex.x, o.vertex.y*scale) + o.vertex.w) * 0.5;
+                    o.uvgrab.zw = o.vertex.zw;
+                    return o;
+                }
+
+                sampler2D _GrabTexture;
+                float4 _GrabTexture_TexelSize;
+                float _Radius;
+
+                half4 frag(v2f i) : COLOR
+                {
+
+                    half4 sum = half4(0,0,0,0);
+                    float radius = 1.41421356237 * _Radius;
+
+                    #define GRABXYPIXEL(kernelx, kernely) tex2Dproj( _GrabTexture, UNITY_PROJ_COORD(float4(i.uvgrab.x + _GrabTexture_TexelSize.x * kernelx, i.uvgrab.y + _GrabTexture_TexelSize.y * kernely, i.uvgrab.z, i.uvgrab.w)))
+
+                    sum += GRABXYPIXEL(0.0, 0.0);
+                    int measurments = 1;
+
+                    for (float range = 1.41421356237f; range <= radius * 1.41; range += 1.41421356237f)
                     {
                         sum += GRABXYPIXEL(range, 0);
                         sum += GRABXYPIXEL(-range, 0);
                         sum += GRABXYPIXEL(0, range);
                         sum += GRABXYPIXEL(0, -range);
-                        measurements += 4;
+                        measurments += 4;
                     }
 
-                    return sum / measurements;
+                    return sum / measurments;
                 }
                 ENDCG
             }

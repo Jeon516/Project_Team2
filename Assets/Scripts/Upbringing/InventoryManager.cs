2a1fb1052cff2697563e4b53876d856bed987c3d
JasonJeon <51731911+Jeon516@users.noreply.github.com>
2023-07-27 00:25:57 +0900

    Update InventoryManager.cs

--- Assets/Scripts/Upbringing/InventoryManager.cs
+++ Assets/Scripts/Upbringing/InventoryManager.cs
@@ -32,7 +32,7 @@ public class InventoryManager : MonoBehaviour
     public List<Button> uiButtonList;
     public List<Image> uiImageList;
     public List<Text> uiTextList;
-    public Text uiItemTextComponent; // Renamed variable to avoid naming conflicts
+    public Text uiItemTextComponent;
     public Image uiItemImage;
     public Button yesButton;
     public Button noButton;
@@ -52,11 +52,9 @@ public class InventoryManager : MonoBehaviour
 
     void OnEnable()
     {
-        // JSON 파일을 읽어서 데이터를 파싱하고 UI 업데이트
         StartCoroutine(LoadDataAndUpdateUI());
 
 #if UNITY_EDITOR
-        // 활성화될 때마다 JSON 파일의 변경을 감지하여 UI 업데이트
         StartFileWatcher();
 #endif
     }
@@ -64,21 +62,18 @@ public class InventoryManager : MonoBehaviour
     void OnDisable()
     {
 #if UNITY_EDITOR
-        // 비활성화될 때 파일 감시 종료
         StopFileWatcher();
 #endif
     }
 
     void Start()
     {
-        // 버튼 리스트에 이벤트 리스너 추가
         for (int i = 0; i < uiButtonList.Count; i++)
         {
-            int index = i; // 이벤트 클로저에 사용할 인덱스 변수
+            int index = i;
             uiButtonList[i].onClick.AddListener(() => OnButtonClicked(index));
         }
 
-        // YesButton과 NoButton에 이벤트 리스너 추가
         yesButton.onClick.AddListener(OnYesButtonClicked);
         noButton.onClick.AddListener(OnNoButtonClicked);
     }
@@ -98,7 +93,6 @@ public class InventoryManager : MonoBehaviour
     {
         if (fileWatcher != null)
         {
-            // 파일 감시 정리
             fileWatcher.Changed -= OnFileChanged;
             fileWatcher.Dispose();
             fileWatcher = null;
@@ -107,65 +101,52 @@ public class InventoryManager : MonoBehaviour
 
     private void OnFileChanged(object sender, FileSystemEventArgs e)
     {
-        // 파일이 변경되면 데이터를 다시 파싱하고 UI를 업데이트
         StartCoroutine(LoadDataAndUpdateUI());
     }
 #endif
 
     private IEnumerator LoadDataAndUpdateUI()
     {
-        // 비동기적으로 JSON 파일을 읽어서 데이터를 파싱
         yield return LoadDataFromJsonFile();
-
-        // UI 업데이트
         UpdateUI();
     }
 
-    private async Task LoadDataFromJsonFile()
+    private IEnumerator LoadDataFromJsonFile()
     {
-        string filePath = Path.Combine(Application.dataPath, "Resources", jsonFilePath + ".json");
-
-        if (File.Exists(filePath))
+        TextAsset jsonTextAsset = Resources.Load<TextAsset>(jsonFilePath);
+        if (jsonTextAsset != null)
         {
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                string jsonData = await reader.ReadToEndAsync();
-                inventoryData = JsonUtility.FromJson<Inventory>(jsonData);
-            }
+            inventoryData = JsonUtility.FromJson<Inventory>(jsonTextAsset.text);
         }
         else
         {
             Debug.LogError("JSON file not found at path: " + jsonFilePath);
         }
+
+        yield return null;
     }
 
     private void UpdateUI()
     {
-        // Handle null check for UI elements before updating UI
         if (uiImageList == null || uiTextList == null || uiItemTextComponent == null || uiItemImage == null)
         {
             Debug.LogError("UI elements not assigned in the Inspector!");
             return;
         }
 
-        // 파싱된 데이터를 기반으로 UI 업데이트
         for (int i = 0; i < uiImageList.Count; i++)
         {
             if (i < inventoryData.itemList.Count)
             {
                 InvenData item = inventoryData.itemList[i];
-
-                // 2. UI이미지 리스트에는 Assets/Resources/Image/Food/[item.MyClass]/[item.imageName].PNG가 할당되어야 함
                 string imagePath = "Image/Food/" + item.myClass + "/" + item.imageName;
-                //stat
                 firstStatType = item.firstStatType;
                 firstStatValue = item.firstStatValue;
                 secondStatType = item.secondStatType;
-                secondStatValue = item.secondStatValue; // Get data
+                secondStatValue = item.secondStatValue;
                 Sprite sprite = Resources.Load<Sprite>(imagePath);
                 if (sprite != null)
                 {
-                    // 3. 이미지 리스트 인덱스가 n이면 이미지 리스트 n번째 자리에 할당
                     uiImageList[i].sprite = sprite;
                 }
                 else
@@ -173,32 +154,23 @@ public class InventoryManager : MonoBehaviour
                     Debug.LogError("Image not found at path: " + imagePath);
                 }
 
-                // 4. 텍스트 리스트에 [item.quantity]를 할당
                 uiTextList[i].text = "" + item.quantity;
             }
             else
             {
-                // 리스트 인덱스가 아이템 개수보다 많으면 비우기
                 uiImageList[i].sprite = null;
                 uiTextList[i].text = string.Empty;
             }
         }
 
-        // 추가: UIText 요소 업데이트
         UpdateUIItemText();
     }
 
     private void OnButtonClicked(int index)
     {
-        // ... (other parts of the code)
-
-        // 아이템 사용 중인지 여부를 확인하고 usingItem 값을 설정
         inventoryData.usingItem = (index >= 0 && index < inventoryData.itemList.Count) ? inventoryData.itemList[index].itemName : string.Empty;
-
-        // 추가: usingItem의 인덱스에 해당하는 itemText 값을 uiItemTextComponent 요소에 할당
         UpdateUIItemText();
 
-        // 추가: usingItem의 인덱스에 해당하는 이미지를 uiItemImage 요소에 할당
         if (index >= 0 && index < uiImageList.Count && index < inventoryData.itemList.Count)
         {
             uiItemImage.sprite = uiImageList[index].sprite;
@@ -211,29 +183,19 @@ public class InventoryManager : MonoBehaviour
 
     private void OnYesButtonClicked()
     {
-        // ... (other parts of the code)
-
-        // 추가: usingItem 초기화
         inventoryData.usingItem = string.Empty;
-        // UI 업데이트
         UpdateUI();
-        // StatChange
         StatChange();
-        Debug.Log(firstStatType + "에서 " + firstStatValue+"만큼 증가하였습니다");
+        Debug.Log(firstStatType + "에서 " + firstStatValue + "만큼 증가하였습니다");
         Debug.Log(secondStatType + "에서 " + secondStatValue + "만큼 증가하였습니다");
     }
 
     private void OnNoButtonClicked()
     {
-        // ... (other parts of the code)
-
-        // 추가: usingItem 초기화
         inventoryData.usingItem = string.Empty;
-        // UI 업데이트
         UpdateUI();
     }
 
-    // 추가: UIText 요소 업데이트
     private void UpdateUIItemText()
     {
         if (!string.IsNullOrEmpty(inventoryData.usingItem))
